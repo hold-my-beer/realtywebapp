1233c0bab596347a0a0ec633fd9005e93be3b1ca
Sergei Petrov <sergey.otis80@gmail.com>
2020-03-01 21:50:58 +0300

    User and Profile schemas created

---
+++ models/Profile.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  secondName: {
+    type: String
+  },
+  dateOfBirth: {
+    type: Date
+  },
+  phoneNumber: {
+    type: String
+  },
+  userPhoto: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'userPhoto'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
