822cda8ec4dfd99d92efd47c4ca5358f6cda7f82
Sergei Petrov <sergey.otis80@gmail.com>
2020-03-02 20:28:19 +0300

    Routes initialized

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 connectDB();
 
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
