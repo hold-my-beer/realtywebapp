748c6b8ce18c23772c33631939342921684f1062
Sergei Petrov <sergey.otis80@gmail.com>
2020-03-03 21:26:42 +0300

    Login route completed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,27 +6,17 @@ const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
-const Profile = require('../../models/Profile');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/auth
+// @desc    Authenticate user & Get token
 // @access  Public
-router.get('/', (req, res) => res.send('users'));
-
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
-
 router.post(
   '/',
   [
-    check('firstName', 'Пожалуйста, укажите ваше имя')
-      .not()
-      .isEmpty(),
     check('email', 'Введенные данные не являются email').isEmail(),
-    check('password', 'Пароль должен содержать не менее 6 символов').isLength({
-      min: 6
-    })
+    check('password', 'Введите пароль')
+      .not()
+      .isEmpty()
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -34,32 +24,25 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { firstName, secondName, email, password } = req.body;
+    const { email, password } = req.body;
 
     try {
       // See if user exists
       let user = await User.findOne({ email });
 
-      if (user) {
+      if (!user) {
         return res
           .status(400)
           .json({ errors: [{ msg: 'Email или пароль указаны неверно' }] });
       }
 
-      user = new User({ email, password });
+      const isMatch = await bcrypt.compare(password, user.password);
 
-      // Encrypt the password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      let profile = new Profile({
-        user: user.id,
-        firstName,
-        secondName
-      });
-      await profile.save();
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Email или пароль указаны неверно' }] });
+      }
 
       // Return jsonwebtoken
       const payload = {
@@ -79,7 +62,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).json({ msg: 'Server Error' });
+      return res.status(500).json({ msg: 'Server Error' });
     }
   }
 );
