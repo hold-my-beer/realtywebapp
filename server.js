d3cad9ac6fdf74992deb8e883416330a910b5618
Sergei Petrov <sergey.otis80@gmail.com>
2020-07-15 18:30:36 +0300

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/proposals', require('./routes/api/proposals'));
 app.use('/api/search', require('./routes/api/search'));
 app.use('/api/provinces', require('./routes/api/provinces'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
